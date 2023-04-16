14b49ac004dfe7f102e12783c25bddc7ccb9603a
vanhocvp <vanhoc2kvp@gmail.com>
2023-04-16 07:21:16 +0700

    add voiceBio

--- controllers/voice_bio.go
+++ controllers/voice_bio.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -9,11 +10,30 @@ import (
 func VoiceBioAuth(c *gin.Context) {
 	file, _ := c.FormFile("file")
 	log.Println(file.Filename)
-
-	// Upload the file to specific dst.
-	c.SaveUploadedFile(file, "file/"+file.Filename)
 	transID := c.PostForm("transaction_id")
 	log.Print(transID)
+
+	// Upload the file to specific dst.
+	//c.SaveUploadedFile(file, "file/"+file.Filename)
+
+	fileData, err := file.Open()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer fileData.Close()
+
+	// Ghi dữ liệu blob vào file mới
+	fileBlob, err := ioutil.ReadAll(fileData)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	err = ioutil.WriteFile("file/"+file.Filename, fileBlob, 0644)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 
 	c.JSON(http.StatusOK, gin.H{
 		"status": 1,
